92c383333547566621e206805a7098cd3cbe5e6e
Craxxxx <mas.didot28@gmail.com>
2025-03-28 16:29:10 +0700

    Implementing Multidimensional array

--- app/src/main/java/learnjavaagain/array.java
+++ app/src/main/java/learnjavaagain/array.java
@@ -2,6 +2,8 @@ package learnjavaagain;
 
 public class array {
 
+
+    //BASIC ARRAY 
     //declaring a basic array
     int [] basicArray = {1, 2, 3, 4, 5};
 
@@ -14,17 +16,18 @@ public class array {
             System.out.println(basicArray[i]);
         }
     }
-
+    //BASIC ARRAY
 
     
-    //declaring a multidimensional array
+    //MULTIDIMENSIONAL ARRAY
+    //declaring a multidimensional array or in this case jagged array
     int [][] mult = { {1, 2, 3, 4}, {5, 6, 7} };
     //please visualize this array for me
     // 1 2 3 4
     // 5 6 7
+    //MULTIDIMENSIONAL ARRAY
 
-
-    //accessing the array manually
+    //ACCESSING THE ARRAY MANUALLY
     public void accessArray()
     {
         System.out.println(mult[0][0]); //1
@@ -35,6 +38,23 @@ public class array {
         System.out.println(mult[1][1]); //6
         System.out.println(mult[1][2]); //7
     }
+    //ACCESSING THE ARRAY MANUALLY
+
+    //ACCESSING THE ARRAY USING LOOPS
+    public void accesArrayLoops()
+    {
+        for (int i=0; i<mult.length; i++)
+        {
+            System.out.println(i);
+            //printing the 1st row
+            //mult[i].length will give the length of the row
+            for (int j=0; j<mult[i].length; j++)
+            {
+                System.out.println(mult[i][j]);
+            }
+        }
+    }
+    //ACCESSING THE ARRAY USING LOOPS
 
 
     //creating main method to access all functions
@@ -48,8 +68,14 @@ public class array {
         System.out.println("This is printing the multidimensional array");
         arr.accessArray(); // Call the accessArray function
 
+        System.out.println("this is printing multidimensional array using loops");
+        arr.accesArrayLoops();
+
         System.out.println("This is printing the basic array");
         arr.printArray(); // Call the printArray function
+
+        System.out.println("MULTIDIMENSIONAL ARRAY LENGTH");
+        System.out.println(arr.mult.length); //2
         
     }
     //accessing the array using a loop
